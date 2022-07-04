0014443ae68f809b72b32a6a7cc68a93e5516e00
Eloagbawe <eloagbawe@gmail.com>
2022-07-04 14:46:55 -0700

    Linked list palindrome

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -3,6 +3,29 @@
 #include "lists.h"
 
 /**
+ * reverse - returns the reversed copy of a list
+ * @head: pointer to the head node
+ * Return: new head
+*/
+listint_t *reverse(listint_t *head)
+{
+	listint_t *new_head = NULL, *temp;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	while (head != NULL)
+	{
+		temp = (listint_t *) malloc(sizeof(listint_t));
+		temp->n = head->n;
+		temp->next = new_head;
+		new_head = temp;
+		head = head->next;
+	}
+	return (new_head);
+}
+/**
  * is_palindrome - checks if a linked list is a palidrome
  * @head: pointer to the head node
  * Return: 0 if it is not a palindrome, 1 if it is a palindrome
@@ -10,17 +33,21 @@
 
 int is_palindrome(listint_t **head)
 {
-	unsigned int len = 1;
-	listint_t *temp;
+	listint_t *temp, *reverse_copy;
 
-	if (head == NULL|| *head == NULL)
+	if (head == NULL || *head == NULL)
 		return (1);
 
 	temp = *head;
-	while (temp)
+	reverse_copy = reverse(*head);
+	while (temp && reverse_copy)
 	{
+		if (temp->n != reverse_copy->n)
+		{
+			return (0);
+		}
 		temp = temp->next;
-		len++;
+		reverse_copy = reverse_copy->next;
 	}
-	return (0);
+	return (1);
 }
