8701c9bb8d0192187180ba17ab15d1c147e4968c
Eloagbawe <eloagbawe@gmail.com>
2022-07-04 16:04:46 +0000

    CPython #0: Python lists

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -9,20 +9,25 @@
 #include <assert.h>
 
 /**
-#include </usr/include/python3.8/object.h>
-#include </usr/include/python3.8/listobject.h>
-*/
-/**
  * print_python_list_info - prints a python list info
  * @p - python list
  * Return: void
 */
 void print_python_list_info(PyObject *p)
 {
-	long int len = PyList_Size(p);
+	Py_ssize_t i, n;
+	PyObject *o;
 
-	printf(len);
-	/**
-	 * printf(Py_SIZE(p))
-	*/
+	n = PyList_Size(p);
+
+	printf("[*] Size of the Python List = %zu\n",n);
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	
+	for (i = 0; i < n; i++)
+	{
+		o = PyList_GetItem(p, i);
+		printf("[*] Element %zu: %s\n",i, Py_TYPE(o)->tp_name);
+	}
+
+
 }
