f1ebd9002d4a8afa2f7eec52f751059c754fd9ae
Eloagbawe <eloagbawe@gmail.com>
2022-07-19 17:43:55 -0700

    update 6-square.py

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,12 +11,12 @@ class Square:
     """Square class
 
         Attributes:
-        attr1(size): size of the square
+        size (int): size of the square
         position (int, int): The position of the new square
     """
     def __init__(self, size=0, position=(0, 0)):
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     @property
     def size(self):
