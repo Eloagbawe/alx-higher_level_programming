bd6a74039dcf9ac889613e985bc9e5c5b432ef6f
Eloagbawe <eloagbawe@gmail.com>
2022-07-27 00:00:30 +0100

    pycodestyle fix

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -22,11 +22,13 @@ def matrix_divided(matrix, div):
 
     """
     new_matrix = []
-  
+
     if not isinstance(matrix, list) or\
-        not all(isinstance(row, list) for row in matrix) or\
-        not all(isinstance(y, (int, float)) for row in matrix for y in row):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+            not all(isinstance(row, list) for row in matrix) or\
+            not all(isinstance(y, (int, float)) for row in matrix for y in row):
+        raise TypeError(
+                'matrix must be a matrix (list '
+                'of lists) of integers/floats')
     elif not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     elif not isinstance(div, (int, float)):
@@ -40,7 +42,7 @@ def matrix_divided(matrix, div):
                 result = round(num / div, 2)
                 new_list.append(result)
             new_matrix.append(new_list)
-    
+
     # if not isinstance(div, (int, float)):
     #     raise TypeError("div must be a number")
     # elif div is 0:
@@ -49,11 +51,13 @@ def matrix_divided(matrix, div):
     #     for elem in matrix:
     #         new_list = []
     #         if len(elem) != len(matrix[0]):
-    #             raise TypeError("Each row of the matrix must have the same size")
+    #             raise TypeError("Each row of\
+    #                the matrix must have the same size")
     #         else:
     #             for num in elem:
     #                 if not isinstance(num, (int, float)):
-    #                     raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    #                     raise TypeError("matrix must be a matrix\
+    #                        (list of lists) of integers/floats")
     #                 else:
     #                     result = round(num / div, 2)
     #                     new_list.append(result)
