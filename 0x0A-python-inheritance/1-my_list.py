0e62895578ba9339252c94b809c528b26e801286
Eloagbawe <eloagbawe@gmail.com>
2022-08-01 20:43:01 +0100

    update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,8 +4,10 @@
 
 
 class MyList(list):
-    """MyList class - a subclass of the list object"""
+    """MyList class - a subclass of the list object
+    """
 
     def print_sorted(self):
-        """Prints a sorted list"""
+        """Prints a sorted list
+        """
         print(sorted(self))
