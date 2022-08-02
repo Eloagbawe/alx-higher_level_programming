675a81f79b3c2d5f45b25dcc30f5904cf0b43e6a
Eloagbawe <eloagbawe@gmail.com>
2022-08-02 21:01:53 +0100

    update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -2,12 +2,9 @@
 """class_to_json module"""
 
 
-import json
-
-
 def class_to_json(obj):
     """returns the dictionary description with simple data structure
         (list, dictionary, string, integer and boolean) for JSON serialization
         of an object
     """
-    return (json.dumps(obj.__dict__))
+    return (obj.__dict__)
