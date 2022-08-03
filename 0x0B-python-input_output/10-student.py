c3beb1208deecefcc81662b6aad634f3c021feb3
Eloagbawe <eloagbawe@gmail.com>
2022-08-03 01:44:16 +0100

    update 10-student.py

--- 0x0B-python-input_output/10-student.py
+++ 0x0B-python-input_output/10-student.py
@@ -11,14 +11,25 @@ class Student:
         self.last_name = last_name
         self.age = age
 
+    # def to_json(self, attrs=None):
+    #     """retrieves a dictionary representation of a Student instance"""
+    #     if attrs is not None and all(isinstance(x, str) for x in attrs):
+    #         new_dict = {}
+    #         for x in attrs:
+    #             for y in self.__dict__:
+    #                 if x == y:
+    #                     new_dict.update({y : self.__dict__[y]})
+    #         return new_dict
+    #     else:
+    #         return self.__dict__
+
     def to_json(self, attrs=None):
-        """retrieves a dictionary representation of a Student instance"""
+        """Retrieve a dictionary representation of a Student instance."""
         if attrs is not None and all(isinstance(x, str) for x in attrs):
-            new_dict = {}
-            for x in attrs:
-                for y in self.__dict__:
-                    if x == y:
-                        new_dict.update({y : self.__dict__[y]})
-            return new_dict
+            d = {}
+            for x, y in self.__dict__.items():
+                if x in attrs:
+                    d[x] = y
+            return d
         else:
             return self.__dict__
