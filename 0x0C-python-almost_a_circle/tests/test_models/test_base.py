b65d7b77e96bf5787b46967f21ac0b2644cf726c
Eloagbawe <eloagbawe@gmail.com>
2022-08-09 01:22:39 +0100

    update tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -5,6 +5,8 @@
 
 import unittest
 from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
 class TestBase(unittest.TestCase):
@@ -21,7 +23,14 @@ class TestBase(unittest.TestCase):
         self.assertEqual(base.id, 100)
 
     def test_to_json_string_valid(self):
-        pass
+        r1 = Rectangle(10, 7, 2, 8)
+        dictionary = r1.to_dictionary()
+        json_dictionary = Base.to_json_string([dictionary])
+        self.assertEqual(json_dictionary, '[{"id": 3, "width": 10, "height": 7, "x": 2, "y": 8}]')
+
+    def test_nb_instances_private(self):
+        with self.assertRaises(AttributeError):
+            print(Base(12).__nb_instances)
 
 
 if __name__ == '__main__':
