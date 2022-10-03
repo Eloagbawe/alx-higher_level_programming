ba8ec3efde4fe7d35bb0037a62bb363e6c0b0c09
Eloagbawe <eloagbawe@gmail.com>
2022-10-03 18:22:07 +0100

    update 11-second_biggest.js

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const argv = process.argv;
-let arr = argv.slice(2);
 
 function convertInt (arr) {
   const newArr = [];
@@ -9,13 +8,21 @@ function convertInt (arr) {
   }
   return (newArr);
 }
-function findSecondBiggest (a) {
+function findSecondBiggest (arr) {
+  const a = arr.slice(2);
   if (a.length === 0 || a.length === 1) {
     return (0);
   }
-  arr = convertInt(a);
-  arr.sort();
-  return (arr[arr.length - 2]);
+  const intArray = convertInt(a);
+  let result;
+  const max = Math.max(...intArray);
+  result = intArray[0];
+  for (let i = 0; i < intArray.length; i++) {
+    if (intArray[i] > result && intArray[i] < max) {
+      result = intArray[i];
+    }
+  }
+  return (result);
 }
 
-console.log(findSecondBiggest(arr));
+console.log(findSecondBiggest(argv));
