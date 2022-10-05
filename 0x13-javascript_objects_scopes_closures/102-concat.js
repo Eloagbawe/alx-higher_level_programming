a48f11aa359ce319b40cca8df8d40496a5ee84be
Eloagbawe <eloagbawe@gmail.com>
2022-10-05 01:57:28 +0100

    update 102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,9 +2,12 @@
 const fs = require('fs');
 const argv = process.argv;
 
-const content = fs.readFileSync(argv[2], 'utf-8');
-const content2 = fs.readFileSync(argv[3], 'utf-8');
-const fullContent = content + '\n' + content2;
-fs.appendFile(argv[4], fullContent, (err) => {
+function func (err, data) {
   if (err) throw err;
-});
+  fs.appendFile(argv[4], data, (err) => {
+    if (err) throw err;
+  });
+}
+
+fs.readFile(argv[2], 'utf-8', func);
+fs.readFile(argv[3], 'utf-8', func);
