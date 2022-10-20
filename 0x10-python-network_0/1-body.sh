1b56065bbc102de642215a5e16ffc924675957d9
Eloagbawe <eloagbawe@gmail.com>
2022-10-20 21:17:33 +0100

    update 1-body.sh

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# This script takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl -s -X GET "$1"
+# This script takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL -X GET "$1"
