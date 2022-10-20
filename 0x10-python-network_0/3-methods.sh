ef59b30c4876d761efcf9d769194613709342939
Eloagbawe <eloagbawe@gmail.com>
2022-10-20 21:44:00 +0100

    update 3-methods.sh

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL and displays all HTTP methods the server will accept
-curl -sI -X OPTIONS "$1" | sed -n '2 p' | cut -c8-
+curl -sI -X OPTIONS "$1" | grep 'Allow' | cut -c8-
