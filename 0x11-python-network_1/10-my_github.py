f28c07b1d0db3fd69f7e48b5a67d3732086462ba
Eloagbawe <eloagbawe@gmail.com>
2022-10-21 23:20:36 +0100

    update 10-my_github.py

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,6 +9,7 @@ import sys
 if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
-    r = requests.get('https://api.github.com/users/{}'.format(username), data={'auth': password})
+    url = 'https://api.github.com/user'
+    r = requests.get(url, auth=(username, password))
     res = r.json()
     print(res.get('id'))
