d0799d363b1d3cc2639be9eaf802a8dd2f3e8dd7
Eloagbawe <eloagbawe@gmail.com>
2022-11-15 19:32:13 +0100

    Star wars Wedge Antilles

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -7,8 +7,11 @@ request(argv[2], function (err, res, body) {
   const data = JSON.parse(body).results;
   let count = 0;
   for (let i = 0; i < data.length; i++) {
-    if (data[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
-      count++;
+    const characterList = data[i].characters;
+    for (let j = 0; j < characterList.length; j++) {
+      if (characterList[j].includes(18)) {
+        count++;
+      }
     }
   }
   console.log(count);
